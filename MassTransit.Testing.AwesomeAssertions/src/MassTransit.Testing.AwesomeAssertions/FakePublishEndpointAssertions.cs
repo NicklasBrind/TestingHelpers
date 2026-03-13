cf9ef8a3388ab9faff77d838dcd47f5816fad8fb
Nicklas <brind@live.se>
2026-03-13 23:39:24 +0100

    Update contract

--- MassTransit.Testing.AwesomeAssertions/src/MassTransit.Testing.AwesomeAssertions/FakePublishEndpointAssertions.cs
+++ MassTransit.Testing.AwesomeAssertions/src/MassTransit.Testing.AwesomeAssertions/FakePublishEndpointAssertions.cs
@@ -13,17 +13,19 @@ public class FakePublishEndpointAssertions : ReferenceTypeAssertions<FakePublish
 
     protected override string Identifier => nameof(FakePublishEndpoint);
 
-    public AndConstraint<FakePublishEndpointAssertions> HavePublished<T>(string because = "", params object[] becauseArgs) where T : class
+    public AndWhichConstraint<FakePublishEndpointAssertions, IReadOnlyList<T>> HavePublished<T>(string because = "", params object[] becauseArgs) where T : class
     {
+        var messages = Subject.PublishedMessages.OfType<T>().ToList();
+
         AssertionChain.GetOrCreate()
             .BecauseOf(because, becauseArgs)
-            .Given(() => Subject.PublishedMessages.OfType<T>())
-            .ForCondition(messages => messages.Any())
+            .Given(() => messages)
+            .ForCondition(m => m.Count > 0)
             .FailWith("Expected a published message of type {0}{reason}, but found none. Published message types: {1}",
                 typeof(T).Name,
                 Subject.PublishedMessages.Select(m => m.GetType().Name));
 
-        return new AndConstraint<FakePublishEndpointAssertions>(this);
+        return new AndWhichConstraint<FakePublishEndpointAssertions, IReadOnlyList<T>>(this, messages);
     }
 
     public AndConstraint<FakePublishEndpointAssertions> NotHavePublished<T>(string because = "", params object[] becauseArgs) where T : class
@@ -37,48 +39,5 @@ public class FakePublishEndpointAssertions : ReferenceTypeAssertions<FakePublish
                 Subject.PublishedMessages.OfType<T>().Count());
 
         return new AndConstraint<FakePublishEndpointAssertions>(this);
-    }
-
-    public AndConstraint<FakePublishEndpointAssertions> HavePublishedCount<T>(int expectedCount, string because = "", params object[] becauseArgs) where T : class
-    {
-        AssertionChain.GetOrCreate()
-            .BecauseOf(because, becauseArgs)
-            .Given(() => Subject.PublishedMessages.OfType<T>().Count())
-            .ForCondition(count => count == expectedCount)
-            .FailWith("Expected {0} published message(s) of type {1}{reason}, but found {2}",
-                expectedCount,
-                typeof(T).Name,
-                Subject.PublishedMessages.OfType<T>().Count());
-
-        return new AndConstraint<FakePublishEndpointAssertions>(this);
-    }
-
-    public AndWhichConstraint<FakePublishEndpointAssertions, T> HavePublishedSingle<T>(string because = "", params object[] becauseArgs) where T : class
-    {
-        var messages = Subject.PublishedMessages.OfType<T>().ToList();
-
-        AssertionChain.GetOrCreate()
-            .BecauseOf(because, becauseArgs)
-            .Given(() => messages)
-            .ForCondition(m => m.Count == 1)
-            .FailWith("Expected exactly 1 published message of type {0}{reason}, but found {1}",
-                typeof(T).Name,
-                messages.Count);
-
-        return new AndWhichConstraint<FakePublishEndpointAssertions, T>(this, messages.SingleOrDefault()!);
-    }
-
-    public AndWhichConstraint<FakePublishEndpointAssertions, T> HaveLastPublished<T>(string because = "", params object[] becauseArgs) where T : class
-    {
-        var messages = Subject.PublishedMessages.OfType<T>().ToList();
-
-        AssertionChain.GetOrCreate()
-            .BecauseOf(because, becauseArgs)
-            .Given(() => messages)
-            .ForCondition(m => m.Count > 0)
-            .FailWith("Expected a published message of type {0}{reason}, but found none",
-                typeof(T).Name);
-
-        return new AndWhichConstraint<FakePublishEndpointAssertions, T>(this, messages.Last());
     }
 }
--- MassTransit.Testing.AwesomeAssertions/tests/MassTransit.Testing.AwesomeAssertions.Test/HavePublishedTests.cs
+++ MassTransit.Testing.AwesomeAssertions/tests/MassTransit.Testing.AwesomeAssertions.Test/HavePublishedTests.cs
@@ -37,4 +37,74 @@ public class HavePublishedTests
             .Throw<XunitException>()
             .WithMessage("*Expected a published message of type*OrderCreated*but found none*");
     }
+
+    [Fact]
+    public async Task Which_gives_access_to_the_collection()
+    {
+        var endpoint = new FakePublishEndpoint();
+        await endpoint.Publish(new OrderCreated(1, "Alice"));
+        await endpoint.Publish(new OrderCreated(2, "Bob"));
+
+        Action action = () => endpoint.Should()
+            .HavePublished<OrderCreated>()
+            .Which.Should().HaveCount(2);
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task Which_contain_single_with_predicate()
+    {
+        var endpoint = new FakePublishEndpoint();
+        await endpoint.Publish(new OrderCreated(1, "Alice"));
+        await endpoint.Publish(new OrderCreated(2, "Bob"));
+
+        Action action = () => endpoint.Should()
+            .HavePublished<OrderCreated>()
+            .Which.Should().ContainSingle(x => x.CustomerName == "Alice");
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task Which_contain_with_predicate()
+    {
+        var endpoint = new FakePublishEndpoint();
+        await endpoint.Publish(new OrderCreated(1, "Alice"));
+        await endpoint.Publish(new OrderCreated(2, "Bob"));
+
+        Action action = () => endpoint.Should()
+            .HavePublished<OrderCreated>()
+            .Which.Should().Contain(x => x.OrderId == 2);
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task Which_only_contain()
+    {
+        var endpoint = new FakePublishEndpoint();
+        await endpoint.Publish(new OrderCreated(1, "Alice"));
+        await endpoint.Publish(new OrderCreated(2, "Bob"));
+
+        Action action = () => endpoint.Should()
+            .HavePublished<OrderCreated>()
+            .Which.Should().OnlyContain(x => x.OrderId > 0);
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task Which_last_element()
+    {
+        var endpoint = new FakePublishEndpoint();
+        await endpoint.Publish(new OrderCreated(1, "Alice"));
+        await endpoint.Publish(new OrderCreated(2, "Bob"));
+
+        Action action = () => endpoint.Should()
+            .HavePublished<OrderCreated>()
+            .Which.Last().CustomerName.Should().Be("Bob");
+
+        action.Should().NotThrow();
+    }
 }
